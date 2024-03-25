12aa36efa3e19167a17fad18fb0533dd979bb1fb
WilliamAxtell <111174139+WilliamAxtell@users.noreply.github.com>
2024-03-25 14:08:18 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+//routes
+app.get('/npc-lookup', (req, res) => {
+    res.send('NPC Lookup');
+});
+
+// app.get('/api/v1/tasks') - get all tasks
+// app.post('/api/v1/tasks') - create a new task
+// app.get('/api/v1/tasks/:id') - get a single task
+// app.patch('/api/v1/tasks/:id') - update a task
+// app.delete('/api/v1/tasks/:id') - delete a task
+
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
